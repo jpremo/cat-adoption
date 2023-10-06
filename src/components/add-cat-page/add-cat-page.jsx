53b4050d17104dc17796aaedda16c399034991d7
Wilson Akins <Wilsonakins.dev@gmail.com>
2023-10-05 23:05:06 -0700

    display cats from old database

--- src/components/add-cat-page/add-cat-page.jsx
+++ src/components/add-cat-page/add-cat-page.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import CatForm from '../cat-form/cat-form';
+import { addCatToNewDB } from '../../utils/utils';
 
 const AddCatPage = () => {
     const onSubmitForm = async (catData) => {
-        console.log('this is the cat data', catData);
+        /*
+        I was trying to submit a fields using the fields paramter to create a single record as mentioned in the documentation.
+        But I think we need to have it like {fields: {fields }} to fix the issue
+        */
+        const results = await addCatToNewDB({fields: catData})
+        //Todo: alert the user in case of success or error
     };
     return (
         <div>
